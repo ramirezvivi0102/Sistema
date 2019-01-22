7eb6ec43ee123ba9040a1967494cb827b5a93752
Viviana Ramirez <46912965+ramirezvivi0102@users.noreply.github.com>
2019-01-21 22:58:12 -0500

    gitignore ide-wall

--- Sistema/Models/Categoria.cs
+++ Sistema/Models/Categoria.cs
@@ -11,7 +11,7 @@ namespace Sistema.Models
         public int CategoriaID { get; set; }
 
         public string Nombre { get; set; }
-        public string Descripcion { get; set; }
+        public string Descripcion { get; set; } 
         public Boolean Estado  { get; set; }
     }
 }
