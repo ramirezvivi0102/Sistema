a5040f7eac459c6bb3b61e0181a6e0197ac19c3b
Viviana Ramirez <46912965+ramirezvivi0102@users.noreply.github.com>
2019-01-21 22:49:05 -0500

    Primer git
    Primer commit - Proyecto ya creado previamente.

---
+++ Sistema/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Boolean Estado  { get; set; }
+    }
+}
